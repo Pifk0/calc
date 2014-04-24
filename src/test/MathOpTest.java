4869f9cae98f10890d3cf5da0b33fe858842255c
ioProject <kriszna14@gmail.com>
2014-04-24 10:44:33 +0200

    first test

---
+++ src/test/MathOpTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class MathOpTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+}
